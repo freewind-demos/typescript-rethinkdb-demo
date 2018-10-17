e45a52c79c8d2de4c1f88945c3f54a7572a11b35
freewind <nowindlee@gmail.com>
2018-10-17 11:07:59 +0800

    simplify code and test id creation

--- hello.ts
+++ hello.ts
@@ -1,50 +1,52 @@
-import {Connection, r} from 'rethinkdb-ts';
+import {r} from 'rethinkdb-ts';
 
 type Todo = {
   task: string
 }
 
-async function insertTasks(conn: Connection) {
+async function insertTasks() {
   console.log('------------- insertTasks -----------------');
   const result = await r.table<Todo>('tasks').insert([
     {
+      id: '111',
       task: 'hello1'
     }, {
       task: 'hello2'
     }
-  ]).run(conn);
+  ]).run();
   console.log(result);
 }
 
-async function createTable(conn: Connection) {
+async function createTable() {
   console.log('------------- createTable -----------------');
-  const result = await r.db('test').tableCreate('tasks').run(conn);
+  const result = await r.db('test').tableCreate('tasks').run();
   console.log(result);
 }
 
-async function loadTasks(conn: Connection) {
+async function loadTasks() {
   console.log('------------- loadTasks -----------------');
-  const result = await r.table('tasks').run(conn);
+  const result = await r.table('tasks').run();
   console.log(result)
 }
 
-async function dropTableIfExist(conn: Connection) {
+async function dropTableIfExist() {
   console.log('------------- dropTableIfExist -----------------');
-  const exists = await r.tableList().contains('tasks').run(conn);
+  const exists = await r.tableList().contains('tasks').run();
   if (exists) {
-    const result = await r.tableDrop('tasks').run(conn);
+    const result = await r.tableDrop('tasks').run();
     console.log(result);
   }
 }
 
 async function start() {
   await r.connectPool({});
-  const conn = await r.connect({});
 
-  await dropTableIfExist(conn);
-  await createTable(conn);
-  await insertTasks(conn);
-  await loadTasks(conn)
+  await dropTableIfExist();
+  await createTable();
+  await insertTasks();
+  await loadTasks();
+
+  await r.getPoolMaster()!.drain()
 }
 
 start();
